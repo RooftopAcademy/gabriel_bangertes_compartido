65fa636a2f36ccb978e8726b705a86fe134d3722
gab <gbangertes@gmail.com>
2021-09-22 14:22:33 -0300

    Added coment view class name.

--- src/view/commentView.js
+++ src/view/commentView.js
@@ -1,6 +1,6 @@
 function commentView(comment) {
     return `
-    <div class="d-flex fd-column">
+    <div class="d-flex fd-column comment">
         <div><b>${comment.name}</b> - <i>${comment.email}</i>
         <div><p>${comment.body}</p>
     </div>`
