05b36bb1d352f9ed8e7df4812ebc0ad24ae3fb0f
gab <gbangertes@gmail.com>
2021-09-25 18:58:44 -0300

    Added types to product view.

---
+++ src/view/productView.ts
@@ -0,0 +1,17 @@
+function productView(product: Product): string {
+    return `
+        <div class='d-flex products-item jc-space-evenly ai-center' product-id='${product.id}'>
+            <div>
+                <img src='${product.img}' class='product-img'>
+            </div>
+            <div class='price'>
+                <h2>$ ${product.price}</h2>
+                <p>${product.title}</p>
+            </div>
+            <div>
+                <button class="cart-button" data-id='${product.id}'>AGREGAR AL CARRITO</button>
+            </div>
+        </div>
+        `;
+}
+
