4a1da3a05e904546aa00aafa084e2ec5728a2b7c
gab <gbangertes@gmail.com>
2021-09-25 18:58:14 -0300

    Added types to comment view.

---
+++ src/view/commentView.ts
@@ -0,0 +1,7 @@
+function commentView(comment: CommentInterface): string {
+    return `
+    <div class="d-flex fd-column comment">
+        <div><b>${comment.name}</b> - <i>${comment.email}</i>
+        <div><p>${comment.body}</p>
+    </div>`
+}
