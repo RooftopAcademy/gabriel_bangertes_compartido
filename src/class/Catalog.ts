a8b733a7b6640b5b9147c1252c0fd72e5a412c54
gab <gbangertes@gmail.com>
2021-09-25 18:56:02 -0300

    Added types to catalog class.

---
+++ src/class/Catalog.ts
@@ -0,0 +1,20 @@
+class Catalog {
+
+    private _productList: Product[];
+
+    constructor() {
+        this._productList = [];
+    }
+
+    addProduct(product: Product) {
+        this._productList.push(product);
+    }
+
+    get productList(): Product[] {
+        return this._productList;
+    }
+
+    findProduct(id: number): Product {
+        return this._productList.find((product: Product) => product.id == id) as Product;
+    }
+}
