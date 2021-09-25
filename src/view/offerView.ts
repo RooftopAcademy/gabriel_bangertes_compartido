bd3b641c62dd9e19f5789c099e554eab20b25bfd
gab <gbangertes@gmail.com>
2021-09-25 18:58:25 -0300

    Added types to offer class.

---
+++ src/view/offerView.ts
@@ -0,0 +1,12 @@
+function offerView(offer: Product): string {
+    return `
+        <div class='d-flex offers-item fd-column jc-space-evenly ai-center"' data-id='${offer.id}'>
+                <img src='${offer.img}' class='offer-img'>
+            <div class='price'>
+                <h2>$ ${offer.price}</h2>
+                <p>${offer.title}</p>
+                <button class="cart-button">AGREGAR AL CARRITO</button>
+            </div>
+        </div>
+        `;
+}
