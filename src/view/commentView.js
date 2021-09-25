239ea0cbfcd5a0dfb071e591b0cbf2fcd6c98b86
gab <gbangertes@gmail.com>
2021-09-25 19:08:14 -0300

    Deleted old javascript files.

--- src/view/commentView.js
+++
@@ -1,7 +0,0 @@
-function commentView(comment) {
-    return `
-    <div class="d-flex fd-column comment">
-        <div><b>${comment.name}</b> - <i>${comment.email}</i>
-        <div><p>${comment.body}</p>
-    </div>`
-}
