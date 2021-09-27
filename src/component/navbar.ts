26fa521e50d02d9b5eb99c9846b959f303b13583
gab <gbangertes@gmail.com>
2021-09-27 13:12:47 -0300

    Added navbar component.

---
+++ src/component/navbar.ts
@@ -0,0 +1,10 @@
+export function navbar() {
+    return `
+        <div class="d-flex navbar jc-space-between fw-wrap ai-center">
+            <div class="logotype"><a href="#">LOGO</a></div>
+            <div class="navbar-item"><a href="#" class="js-home">Home</a></div>
+            <div class="navbar-item"><a href="#" class="js-products">Productos</a></div>
+            <div class="search"><a href="#">BUSCAR</a></div>
+            <div class="menu"><a href="#" class="hamburger-btn"><i class="fas fa-bars"></i></a></div>
+        </div>`
+}
