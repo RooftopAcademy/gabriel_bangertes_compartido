2c42fd5584326fad5fd19a8c8fe85fda87b340c7
gbangertes <gbangertes@gmail.com>
2021-10-13 16:05:29 -0300

    Added CartItem class.

---
+++ src/entities/CartItem.ts
@@ -0,0 +1,25 @@
+import Product from "./Product";
+
+export default class CartItem {
+
+    private product: Product;
+
+    constructor(product: Product) {
+        this.product = product;
+        this.quantity = 1;
+    }
+
+    private quantity: number;
+
+    increaseQuantity(quantity: number = 1): void {
+        this.quantity += quantity;
+    }
+
+    decreaseQuantity(quantity: number = 1): void {
+        this.quantity -= quantity;
+    }
+
+    getQuantity(): number {
+        return this.quantity;
+    }
+}
