b3f23ea8fb1020c3085a5499989f45084225ed62
gbangertes <gbangertes@gmail.com>
2021-10-14 09:35:11 -0300

    Added cart view.

---
+++ src/views/cartView.ts
@@ -0,0 +1,12 @@
+import Cart from "../entities/Cart";
+
+export default function cartView(cart: Cart): string {
+    return `
+        <div class="d-flex fd-column">
+            <div class="cart-items"></div>
+            <div class="check-out-button">
+                <button>CONTINUAR COMPRA</button>
+            </div>
+        </div>
+`;
+}
