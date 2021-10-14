fd86ecff5c91f1bd0e337dd121152a574eb549c3
gbangertes <gbangertes@gmail.com>
2021-10-14 20:24:15 -0300

    Added plus and minor buttons to cart item component.

--- src/components/cartItemComponent.ts
+++ src/components/cartItemComponent.ts
@@ -2,13 +2,17 @@ import CartItem from "../entities/CartItem";
 
 export default function cartItemComponent(cartItem: CartItem): string {
     return `
-    <div class="d-flex cart-item">
+    <div class="d-flex cart-item jc-space-evenly ai-center">
         <div>
-            <img src="${cartItem.getProduct().getImg()}" alt="${cartItem.getProduct().getTitle()}">
+            <img class="product-img" src="${cartItem.getProduct().getImg()}" alt="${cartItem.getProduct().getTitle()}">
         </div>
         <div>${cartItem.getProduct().getTitle()}</div>
         <div>$ ${cartItem.getProduct().getPrice()}</div>
-        <div>x ${cartItem.getQuantity()} u</div>
+        <div class="d-flex jc-space-evenly ai-center">
+            <div><button class="plus-button button"><icon class="fas fa-minus"></icon></button></div>
+            <div>${cartItem.getQuantity()}</div>
+            <div><button class="minus-button button"><icon class="fas fa-plus"></icon></button></div>
+        </div>
         <div>$ ${cartItem.getProduct().getPrice() * cartItem.getQuantity()}</div>
     </div>
     `;
