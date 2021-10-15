88fce07b4bea0557cc62ffc511efeef984bfc972
gbangertes <gbangertes@gmail.com>
2021-10-15 09:36:36 -0300

    Added cart button to navbar component.

--- src/components/navbarComponent.ts
+++ src/components/navbarComponent.ts
@@ -1,10 +1,10 @@
 export default function navbarComponent() {
     return `
         <div class="d-flex navbar jc-space-between fw-wrap ai-center">
-            <div class="logotype"><a href="#">LOGO</a></div>
-            <div class="navbar-item"><a href="#" class="js-home">Home</a></div>
-            <div class="navbar-item"><a href="#" class="js-products">Productos</a></div>
-            <div class="search"><a href="#">BUSCAR</a></div>
+            <div class="logotype"><a href="#"><img class="logo-img" src="favicon.ico" alt="CARRITO"></a></div>
+            <div class="navbar-item"><a href="#" class="js-home">HOME</a></div>
+            <div class="navbar-item"><a href="#" class="js-products">PRODUCTOS</a></div>
+            <div class="cart"><a href="#" class="cart-button"><i class="ts-cart-icon fas fa-shopping-cart"></i></a></div>
             <div class="menu"><a href="#" class="hamburger-btn"><i class="ts-hamburger-icon fas fa-bars"></i></a></div>
         </div>`
 }
