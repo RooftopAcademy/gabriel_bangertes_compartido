05bd1bbd9a7c4a41c82de94bc83505799a91faef
gbangertes <gbangertes@gmail.com>
2021-10-15 08:46:16 -0300

    Added total price to cart view.

--- src/views/cartView.ts
+++ src/views/cartView.ts
@@ -4,6 +4,13 @@ export default function cartView(cart: Cart): string {
     return `
         <div class="d-flex fd-column ai-center">
             <div class="d-flex cart-items fd-column"></div>
+            <br>
+            <div class="totals d-flex jc-flex-end">
+                <div class="price">
+                    TOTAL $ ${cart.getTotal()}
+                </div>
+            </div>
+            <br>
             <div class="check-out-button">
                 <button class="button">CONTINUAR COMPRA</button>
             </div>
