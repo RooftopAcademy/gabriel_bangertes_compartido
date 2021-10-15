839453a965a32ec7ee93b2319f32ffe8f66ba246
gbangertes <gbangertes@gmail.com>
2021-10-15 08:49:43 -0300

    Fixed view and button styles.

--- src/views/productDetailView.ts
+++ src/views/productDetailView.ts
@@ -2,7 +2,7 @@ import Product from '../entities/Product';
 
 export default function productDetailView(product: Product): string {
     return `
-        <div class='d-flex fd-column products-item jc-space-evenly ai-center' data-id='${product.getId()}'>
+        <div class='d-flex fd-column products-item jc-space-evenly ai-center w-95' data-id='${product.getId()}'>
             <div>
                 <img src='${product.getImg()}' class='offer-img' alt="image">
             </div>
