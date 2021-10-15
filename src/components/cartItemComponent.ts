04896412a745ac248a760f55e1d56608b992660c
gbangertes <gbangertes@gmail.com>
2021-10-14 22:35:50 -0300

    Modified plus and minus buttons.

--- src/components/cartItemComponent.ts
+++ src/components/cartItemComponent.ts
@@ -9,9 +9,9 @@ export default function cartItemComponent(cartItem: CartItem): string {
         <div>${cartItem.getProduct().getTitle()}</div>
         <div>$ ${cartItem.getProduct().getPrice()}</div>
         <div class="d-flex jc-space-evenly ai-center">
-            <div><button class="plus-button button"><icon class="fas fa-minus"></icon></button></div>
+            <div><button class="minus-button button" data-id="${cartItem.getProduct().getId()}"><icon class="fas fa-minus"></icon></button></div>
             <div>${cartItem.getQuantity()}</div>
-            <div><button class="minus-button button"><icon class="fas fa-plus"></icon></button></div>
+            <div><button class="plus-button button" data-id="${cartItem.getProduct().getId()}"><icon class="fas fa-plus"></icon></button></div>
         </div>
         <div>$ ${cartItem.getProduct().getPrice() * cartItem.getQuantity()}</div>
     </div>
